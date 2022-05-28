15fd94a5911e0d77fdcc08efc8ba137bfd6908d2
Fabricio assuncao <fabricioassuncao855@outlook.com>
2022-05-27 22:22:19 -0300

    corrigido os caminhos de arquivos

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
+
 const offcanvas = document.querySelector('.js-offcanvas')
-const btnCanvas = document.querySelector('.btn-canvaOff')
 const colorCanva = document.querySelector('.heade-js')
 const exitCanva = document.querySelector('.exit')
 
+function addCanvas() {
+    document.querySelector('.js-offcanvas').classList.add('canvas-active')
+   
+}
 
-btnCanvas.addEventListener( 'click', ()=> {
-    offcanvas.classList.add('canvas-active')
-    
-})
-exitCanva.addEventListener('click', () => { 
+function exitCanvas() {
     offcanvas.classList.remove('canvas-active')
-})
+    
+}
 
 window.addEventListener('scroll',  ()=> {
     if (window.scrollY > 0) {
         colorCanva.classList.add('canvas-color')
+
    
     }
     if (window.scrollY == 0) {
@@ -23,3 +25,28 @@ window.addEventListener('scroll',  ()=> {
   
 
 })
+
+function emailjsValid() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('3oqTYKzgKx5zeyP4V');
+}
+
+emailjsValid()
+
+
+
+function sendEmail() {
+    document.querySelector('.form').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        // this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('service_jgkj0my', 'template_7v6s6ez', this)
+            .then(function(text) {
+                alert('Your message has been sent!');
+            }, function(error) {
+                alert('ocorreu um erro inesperado! tente novamente mais tarde.');
+            });
+    }
+)};
+
+window.onload = sendEmail();
